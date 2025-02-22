3a2c89f3e931e1fb36c3c87c97c847fe9006d11b
marko <markopopov1993@gmail.com>
2025-02-22 09:56:03 +0100

    Initial commit

---
+++ src/main/java/com/maximus/Airport_Gate_Management_System/AirportGateManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.maximus.Airport_Gate_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportGateManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportGateManagementSystemApplication.class, args);
+	}
+
+}
